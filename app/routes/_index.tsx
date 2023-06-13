7e0a05084c6021cae34ac1bad7ae312248e3f485
ryu <114303361+ryuapp@users.noreply.github.com>
2023-06-13 19:59:10 +0900

    💄 Update title

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -3,40 +3,15 @@ import type { V2_MetaFunction } from "@remix-run/deno";
 
 export const meta: V2_MetaFunction = () => {
   return [
-    { title: "New Remix App" },
+    { title: "Deno BBS" },
     { name: "description", content: "Welcome to Remix!" },
   ];
 };
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="mx-auto max-w-5xl mt-2 font-mono">
+      <h1 className="text-3xl font-bold">Deno BBS</h1>
     </div>
   );
 }
